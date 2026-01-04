0253215624af8db6694b87db18ea5a6932cf08ee
Ally <allymaclennan91@gmail.com>
2026-01-04 17:16:14 +0000

    terraform test

---
+++ src/terraform_test.py
@@ -0,0 +1,27 @@
+from tofupy import Tofu
+import os
+
+def main(terraform_dir: str):
+    print("Testing terraform..")
+    workspace = Tofu(
+        cwd=(terraform_dir),
+        binary="terraform",
+    )
+    
+    print("Running terraform format..")
+    fmt = workspace._run(args=["fmt"])
+    print(f"{fmt}")
+
+    workspace.init(disable_backends=True)
+
+    # Validate configuration
+    validation = workspace.validate()
+    if not validation.valid:
+        print("Configuration is invalid!")
+        for diagnostic in validation.diagnostics:
+            print(f"Error: {diagnostic.summary}")
+    else:
+        print("Terraform is valid..")
+
+if __name__ == "__main__":
+    print("no directo")
