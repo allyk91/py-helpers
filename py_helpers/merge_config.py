c2d2c84396e15589d0763e30a3273bb329afed1b
Ally <allap@local>
2026-01-05 23:39:49 +0000

    merge

--- py_helpers/merge_config.py
+++ py_helpers/merge_config.py
@@ -1,20 +1,21 @@
 import yaml
+import os
 
 
 def get_config():
     print("Hello from py-helpers!")
 
 
-def merge_dicts(*dicts):
+def merge_config(*config_files):
     d = {}
-    for dict in dicts:
-        for key in dict:
-            try:
-                d[key] = dict[key]
-            except KeyError:
-                print(f"unable to update {key} in {dict}")
+    for f in config_files:
+        try:
+            with open(f) as config:
+                c = yaml.safe_load(config)
+                d = d | c
+        except:
+            print("Fail to load config")
     return d
-
 
 def test():
     print("testing!")
@@ -25,26 +26,10 @@ if __name__ == "__main__":
     d2 = {"c": 3, "b": 4}
     d3 = {"a": 5, "d": 6}
 
-    try:
-        with open("config.yml") as config:
-            c1 = yaml.safe_load(config)
-    except:
-        print("Fail to load config")
-
-    try:
-        with open("global_config.yml") as config:
-            c2 = yaml.safe_load(config)
-    except:
-        print("Fail to load config")
-
-    try:
-        with open("workload.yml") as config:
-            c3 = yaml.safe_load(config)
-    except:
-        print("Fail to load config")
-
-    # print(c1)
-    # print(c2)
-    # new_d = merge_dicts(c2, c3, c1)
-    new_d = c2 | c3 | c1
+    c1 = os.path.join(os.getcwd(), "config.yml")
+    c2 = os.path.join(os.getcwd(), "global_config.yml")
+    c3 = os.path.join(os.getcwd(), "workload.yml")
+    print(c2)
+    new_d = merge_config(c1, c2, c3)
+    # new_d = c2 | c3 | c1
     print(new_d)
